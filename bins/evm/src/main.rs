6400754f757be13585761a992d9a833aa3a65de4
blurbeast <leumasre@gmail.com>
2025-11-03 15:24:21 +0100

    updated

--- bins/evm/src/main.rs
+++ bins/evm/src/main.rs
@@ -1,3 +1,16 @@
+use evm_core::Evm;
+use primitives::{evm_types::{BlockEnv, EvmStorage, Transaction}, memory::Memory, stack::Stack};
+
 fn main() {
-    println!("Hello, world!");
+    
+    let block_env = BlockEnv::default();
+    let memory = Memory::default();
+    let stack = Stack::default();
+    let storage = EvmStorage::default();
+    
+    let tx: Transaction = Transaction::default();
+    
+    
+    let mut evm = Evm::new(block_env, tx, memory, stack, storage);
+    // println!("Hello, world!");
 }
